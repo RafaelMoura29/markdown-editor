5bf035bad8c6c4b6ecaaa70f9a8fdb7c6b2a264d
rafael moura <rafaelmoura2929@gmail.com>
2020-05-03 20:14:51 -0300

    Adicionando preact no bundle de produção

--- webpack/prod.config.js
+++ webpack/prod.config.js
@@ -33,7 +33,8 @@ module.exports = {
       name: 'react-build',
       chunks: ['main'],
       minChunks: ({ resource }) => (
-        /node_modules\/react(-dom)?|fbjs\//.test(resource)
+        /node_modules\/react(-dom)?|fbjs\//.test(resource) ||
+        /node_modules\/preact/.text(resource)
       )
     }),
 
@@ -46,13 +47,13 @@ module.exports = {
     }),
 
     new HtmlPlugin(Object.assign({}, common.htmlPluginConfig, {
-      minify: {collapseWhitespace: true},
+      minify: { collapseWhitespace: true },
 
       chunksSortMode: (chunk1, chunk2) => {
         const order = ['react-build', 'vendor', 'main']
         const left = order.indexOf(chunk1.names[0])
         const right = order.indexOf(chunk2.names[0])
-        return left - right 
+        return left - right
       }
     })),
 
@@ -78,5 +79,10 @@ module.exports = {
     ]
   },
 
-  resolve: common.resolve
+  resolve: {
+    alias: Object.assign({}, common.resolve.alias, {
+      'react': 'preact-compat',
+      'react-dom': 'preact-compat'
+    })
+  }
 }
